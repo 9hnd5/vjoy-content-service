7d2dcdeb133bb77ed22c31e77ed8142a06c4d920
9hnd5 <huy.nguyendinh@vus-etsc.edu.vn>
2023-06-28 12:45:06 +0700

    cloud build staging

--- src/entities/kid-data.entity.ts
+++ src/entities/kid-data.entity.ts
@@ -75,14 +75,14 @@ export class KidData extends Model<KidDataAttributes, KidDataCreationAttributes>
   kidLessons: KidLesson[];
 
   @AfterFind
-  static async calculateEnergy(instance: KidData) {
+  static calculateEnergy(instance: KidData) {
     if (instance) {
       const minutes = dayjs(new Date()).diff(instance?.lastUpdatedEnergy, "minutes");
       if (minutes >= 5) {
         const newEnergy = minutes * ENERGY_PER_MINUTE + instance.energy;
         instance.energy = Math.floor(newEnergy >= MAX_ENERGY ? MAX_ENERGY : newEnergy);
         instance.lastUpdatedEnergy = new Date();
-        await instance.save();
+        instance.save();
       }
     }
   }
