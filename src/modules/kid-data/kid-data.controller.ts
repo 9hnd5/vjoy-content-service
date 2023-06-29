acba7dbcc3b14cb13f5fe7889b7a808049ac4cf2
9hnd5 <huy.nguyendinh@vus-etsc.edu.vn>
2023-06-29 14:02:42 +0700

    Change resource to kid-data, kid-lesson

--- src/modules/kid-data/kid-data.controller.ts
+++ src/modules/kid-data/kid-data.controller.ts
@@ -10,37 +10,37 @@ import { KidDataService } from "./kid-data.service";
 export class KidDataController {
   constructor(private kidDataService: KidDataService) {}
 
-  @Authorize({ resource: "kid-learning-data", action: "create" })
+  @Authorize({ resource: "kid-data", action: "create" })
   @Post("kid-data")
   async createKidData(@Body() data: CreateKidDataDto) {
     return this.kidDataService.create(data);
   }
 
-  @Authorize({ resource: "kid-learning-data", action: "update" })
+  @Authorize({ resource: "kid-data", action: "update" })
   @Patch("kid-data/:kidId")
   async updateKidData(@Param("kidId") kidId: number, @Body() data: UpdateKidDataDto) {
     return this.kidDataService.update(kidId, data);
   }
 
-  @Authorize({ resource: "kid-learning-data", action: "read" })
+  @Authorize({ resource: "kid-data", action: "read" })
   @Get("kid-data/:kidId/energy")
   async getKidDataEnergy(@Param("kidId") kidId: number) {
     return this.kidDataService.getEnergy(kidId);
   }
 
-  @Authorize({ resource: "kid-learning-data", action: "update" })
+  @Authorize({ resource: "kid-data", action: "update" })
   @Post("kid-data/:kidId/energy")
   async buyKidDataEnergy(@Param("kidId") kidId: number) {
     return this.kidDataService.buyEnergy(kidId);
   }
 
-  @Authorize({ resource: "kid-learning-data", action: "read" })
+  @Authorize({ resource: "kid-data", action: "read" })
   @Get("kid-data/:kidId/star")
   async getKidDataStar(@Param("kidId") kidId: number) {
     return this.kidDataService.getStar(kidId);
   }
 
-  @Authorize({ resource: "kid-learning-data", action: "read" })
+  @Authorize({ resource: "kid-data", action: "read" })
   @Get("kid-data")
   async getKidData() {
     return this.kidDataService.getDataByUser();
